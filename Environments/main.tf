c21ace0c369727e87d996ae8e5f04920d6d0e3a2
maheshlokku <maheshlokku9@gmail.com>
2026-02-26 17:47:10 +0530

    terraform files

---
+++ Environments/main.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+  source    = "./modules/vpc"
+  vpc_cidr  = var.vpc_cidr
+}
+
+module "subnet" {
+  source      = "./modules/subnet"
+  vpc_id      = module.vpc.vpc_id
+  subnet_cidr = var.subnet_cidr
+}
+
+module "ec2" {
+  source        = "./modules/ec2"
+  subnet_id     = module.subnet.subnet_id
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+}
---
+++ Environments/variables.tf
@@ -0,0 +1,4 @@
+variable "region" {}
+variable "vpc_cidr" {}
+variable "subnet_cidr" {}
+variable "instance_type" {}
