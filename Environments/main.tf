b61f88846478031e18abd57cb5f769d4f9ae52cb
maheshlokku <maheshlokku9@gmail.com>
2026-02-26 21:26:00 +0530

    change pipeline

--- Environments/main.tf
+++ Environments/main.tf
@@ -13,6 +13,5 @@ module "ec2" {
   source        = "../modules/ec2"
   vpc_id        = module.vpc.vpc_id
   subnet_id     = module.subnet.subnet_id
-  ami_id        = var.ami_id
   instance_type = var.instance_type
 }
